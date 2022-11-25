5bd443fb899ab632f8aa3c6b179b1e359e858f49
Carolyn Qian <carolyn.qian@berkeley.edu>
2022-11-24 17:53:20 -0800

    Updated UART_echo.py the Nano to send/receive data to/from the ESP32 and for the Nano to also send/receive data to/from a rostopic: * ESP32 -> Nano -> rostopic: sensor data * rostopic -> Nano -> ESP32: commands for chickenbot movement NOTE: This is not complete!! The callback function isn't writing the right values to the ESP32, and we still need to fill out the SensorData values to send to the rostopic.

--- JetsonNano/UART_echo.py
+++ JetsonNano/UART_echo.py
@@ -8,38 +8,54 @@
 # both read and write capabilities
 # https://github.com/JetsonHacksNano/UARTDemo/blob/master/uart_example.py 
 # this stuff ^ (read input) and send to ROS topic
+
 import time
 import serial
-# import rospy
-# from std_msgs.msg import String
+import rospy
+from std_msgs.msg import SensorData, ControllerCommands #TODO: create SensorData and ControllerCommands message types
 
-serial_port = serial.Serial( # opens serial port
-    port="/dev/ttyTHS1",
-    baudrate=115200,
-    bytesize=serial.EIGHTBITS,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    timeout = 10
-)
+def floatToString(value: float):
+    val = ""
+    if value < 10:
+        val = "00"
+    elif value < 100:
+        val = "0"
+    return val + (str (round(value, 2)))
 
-try:
-    # pub = rospy.Publisher('chatter_talk', String, queue_size=10)
-    while True:
+def callback(command):
+    print("command received: ", command)
+    velocity = command.velocity
+    theta_dot = command.theta_dot
+    # send commands to esp32
+    commands_to_sensor = command.state + floatToString(velocity[0]) + floatToString(velocity[1]) + floatToString(theta_dot[0]) + floatToString(theta_dot[1]) + floatToString(command.hip_angle) + floatToString(command.lower_neck_angle) + floatToString(command.upper_neck_angle) + (str (int (round(command.grapser, 0)))) + "\n"
+    serial_port.write(commands_to_sensor)
+
+def nano_interface():
+    r = rospy.Rate(10) # loop runs at 10hz (10 loops / sec)
+    while not rospy.is_shutdown():
         if serial_port.in_waiting() > 0:
-            data = serial_port.read().decode() # reads bytes from serial port and decodes into unicode (defaultf for python)
-            # if data == "\r".encode(): # i still dont entirely get this
-            #     serial_port.write("\n".encode())
-            serial_port.write("Sent by Nano.\n".encode())
-            # pub.publish(data)
+            data = serial_port.read().decode() # reads bytes from serial port and decodes into unicode (default for python)
             print("I received ", data)
+            # TODO: put data into SensorData msg
+            sensorData = SensorData()
+            pub.publish(sensorData)
+        r.sleep()
 
-except KeyboardInterrupt:
-    print("Exiting Program")
+if __name__ == '__main__':
+    serial_port = serial.Serial( # opens serial port
+        port="/dev/ttyTHS1",
+        baudrate=115200,
+        bytesize=serial.EIGHTBITS,
+        parity=serial.PARITY_NONE,                  
+        stopbits=serial.STOPBITS_ONE,
+        timeout = 10
+    )
 
-except Exception as exception_error:
-    print("Error occurred. Exiting Program")
-    print("Error: " + str(exception_error))
-
-finally:
-    serial_port.close()
-    pass
+    rospy.init_node('nano_interface')
+    rospy.Subscriber("controller_commands", ControllerCommands, callback)
+    pub = rospy.Publisher('sensor_data', SensorData, queue_size=10)
+    rospy.on_shutdown(serial_port.close()) # closes the serial port when ROS gets the shutdown signal
+    # Check if the node has received a signal to shut down. If not, run the method.
+    try:
+        nano_interface()
+    except rospy.ROSInterruptException: pass
