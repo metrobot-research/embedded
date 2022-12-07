c1c4899f711bdd2b0574eb06fe6fcb3b9ffc61e2
Class Account <ee106a-afj@c105-2.local>
2022-12-07 11:41:14 -0800

    edited catkin files (ie CMakeLists.txt and package.xml)

--- JetsonNano/src/jetson_nano/src/UART_echo.py
+++ JetsonNano/src/jetson_nano/src/UART_echo.py
@@ -13,7 +13,7 @@ import time
 import serial
 import rospy
 from struct import pack, unpack
-from std_msgs.msg import SensorData, ControllerCommands #TODO: create SensorData and ControllerCommands message types
+from jetson_nano.msg import SensorData, ControllerCommands #TODO: create SensorData and ControllerCommands message types
 
 def callback(command):
     print("command received: ", command)
@@ -22,39 +22,21 @@ def callback(command):
     commands_to_sensor = pack('h', command.velocity) + pack('h', command.theta_dot) + pack('H', command.hip_angle) + pack('H', command.lower_neck_angle) + pack('H', command.upper_neck_angle) + pack('B', command.grasper) + pack('c', command.state)
     serial_port.write(commands_to_sensor)
 
-# def nano_interface():
-#     r = rospy.Rate(10) # loop runs at 10hz (10 loops / sec)
-#     while not rospy.is_shutdown():
-#         if serial_port.in_waiting() > 0:
-#             data = serial_port.read().decode() # reads bytes from serial port and decodes into unicode (default for python)
-#             print("I received ", data)
-#             # put data into SensorData msg
-#             sensorData = SensorData()
-#             sensorData.acceleration = [unpack('h', data[:2]), unpack('h', data[2:4]), unpack('h', data[4:6])]
-#             sensorData.orientation = [unpack('h', data[6:8]), unpack('h', data[8:10]), unpack('h', data[10:12])]
-#             sensorData.wheel_speeds = [unpack('h', data[12:14]), unpack('h', data[14:16])]
-#             sensorData.hip_angles = [unpack('h', data[16:18]), unpack('h', data[18:20])]
-#             sensorData.neck_angle = unpack('h', data[20:22])
-#             sensorData.head_angle = unpack('h', data[22:24])
-#         # Send message: short integer for now:
-#         message = pack('>H',42)
-#         serial_port.write(message)
-#         print("Sent: ",message,end='\n')
-#         r.sleep()
-
-def nano_interface():
+def nano_interface(serial_port):
     buffer = b''
-    while True:
+    r = rospy.Rate(10)
+    while not rospy.is_shutdown():
         # receive
         if serial_port.in_waiting > 0:
             abyte = serial_port.read()
             # print("I received ", Abyte)
 
             if abyte == b'\n':
-                interpret_msg(buffer) # TODO
+                interpret_msg(buffer)
                 buffer = b''
             else:
                 buffer += abyte
+        r.sleep()
         
         # send
         message = pack('<f', 3.1415926) + b'\n'
@@ -70,20 +52,11 @@ def interpret_msg(buffer):
     sensor_msg.acceleration = list(val_tuples[:3])
     sensor_msg.orientation = list(val_tuples[3:6]) #[unpack('h', data[6:8]), unpack('h', data[8:10]), unpack('h', data[10:12])]
     sensor_msg.phi = val_tuples[6]
-    sensor_msg.wheel_speeds = list(val_tuples[7:9]) #[unpack('h', data[12:14]), unpack('h', data[14:16])]
-    sensor_msg.hip_angles = list(val_tuples[9:11]) # [unpack('h', data[16:18]), unpack('h', data[18:20])]
-    sensor_msg.neck_angle = val_tuples[11] # unpack('h', data[20:22])
-    sensor_msg.head_angle = val_tuples[12] # unpack('h', data[22:24])
+    sensor_msg.wheel_speeds = list(val_tuples[7:9]) 
+    sensor_msg.hip_angles = list(val_tuples[9:11]) 
+    sensor_msg.neck_angle = val_tuples[11] 
+    sensor_msg.head_angle = val_tuples[12] 
     rospy.loginfo(sensor_msg)
-    
-def testing_serial():
-    while True:
-        # message = pack('<HH',42, 32) + b'\n'
-        message = pack('<f', 3.1415926) + b'\n'
-        serial_port.write(message)
-        print("Sent: ", message, end='\n')
-        
-        time.sleep(2)
 
 if __name__ == '__main__':
     serial_port = serial.Serial( # opens serial port
@@ -95,17 +68,11 @@ if __name__ == '__main__':
         timeout = 10
     )
 
-    # rospy.init_node('nano_interface')
-    # rospy.Subscriber("controller_commands", ControllerCommands, callback)
-    # pub = rospy.Publisher('sensor_data', SensorData, queue_size=10)
-    # rospy.on_shutdown(serial_port.close()) # closes the serial port when ROS gets the shutdown signal
+    rospy.init_node('nano_interface')
+    rospy.Subscriber("controller_commands", ControllerCommands, callback)
+    pub = rospy.Publisher('sensor_data', SensorData, queue_size=10)
+    rospy.on_shutdown(serial_port.close()) # closes the serial port when ROS gets the shutdown signal
     # Check if the node has received a signal to shut down. If not, run the method.
-    # try:
-    #     nano_interface()
-    # except rospy.ROSInterruptException: pass
-
-    """serial sending test"""
-    # testing_serial()
-
-    """serial receiving test"""
-    nano_interface()
+    try:
+        nano_interface(serial_port)
+    except rospy.ROSInterruptException: pass
