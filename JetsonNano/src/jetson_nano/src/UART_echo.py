ef401061472c3428133f97ec2ccb8dcb88f43bb4
Carolyn Qian <carolyn.qian@berkeley.edu>
2022-12-07 15:41:23 -0800

    fixed bug (moved the publisher in the UART_echo.py file, then edited cmd.msg with the right values

--- JetsonNano/src/jetson_nano/src/UART_echo.py
+++ JetsonNano/src/jetson_nano/src/UART_echo.py
@@ -20,10 +20,12 @@ def callback(command):
     print("command received: ", command)
     # send commands to esp32 
     # velocity, theta_dot are shorts. hip_angle, lower_neck_angle, upper_neck_angle are unsigned shorts. grasper is an unsigned short. state is a char.
-    commands_to_sensor = pack('<chhHH', command.state, command.velocity, command.theta_dot, command.hipAngle, command.lower_neck_angle, command.upper_neck_vel, command.grasperVel)
+    command.neckPosition = 0
+    commands_to_sensor = pack('<ccHfffHH', command.state, command.furtherState, command.x_dot, command.theta_dot, command.hipAngle, command.neckPosition, command.headVelocity, command.grasperVelocity)
     serial_port.write(commands_to_sensor)
 
 def nano_interface(serial_port):
+    pub = rospy.Publisher('sensor_data', SensorData, queue_size=10)
     buffer = b''
     r = rospy.Rate(20)
     while not rospy.is_shutdown():
@@ -32,15 +34,13 @@ def nano_interface(serial_port):
             abyte = serial_port.read()
             # print("I received ", Abyte)
             if abyte == b'\n':
-                interpret_msg(buffer)
+                sensor_msg = interpret_msg(buffer)
+                if sensor_msg:
+                    pub.publish(sensor_msg)
                 buffer = b''
             else:
                 buffer += abyte
         r.sleep()
-        
-        # send
-        message = pack('<f', 3.1415926) + b'\n'
-        serial_port.write(message)
 
 def interpret_msg(buffer): 
     if(len(buffer) != 56): 
@@ -50,17 +50,19 @@ def interpret_msg(buffer):
     val_tuples = unpack('<ffffffffffffHHc', buffer)
     sensor_msg = SensorData()
     sensor_msg.acceleration = list(val_tuples[:3])
-    sensor_msg.orientation = list(val_tuples[3:6]) #[unpack('h', data[6:8]), unpack('h', data[8:10]), unpack('h', data[10:12])]
+    sensor_msg.orientation = list(val_tuples[3:6]) 
     sensor_msg.phi = val_tuples[6]
     sensor_msg.wheel_speeds = list(val_tuples[7:9]) 
     sensor_msg.hip_angles = list(val_tuples[9:11]) 
     sensor_msg.neck_angle = val_tuples[11] 
     sensor_msg.head_angle = val_tuples[12] 
     rospy.loginfo(sensor_msg)
+    return sensor_msg
+    
 
 if __name__ == '__main__':
     serial_port = serial.Serial( # opens serial port
-        port="/dev/cu.SLAB_USBtoUART",
+        port="/dev/ttyTHS1",
         baudrate=115200,
         bytesize=serial.EIGHTBITS,
         parity=serial.PARITY_NONE,                  
@@ -70,7 +72,6 @@ if __name__ == '__main__':
 
     rospy.init_node('nano_interface')
     rospy.Subscriber("controller_commands", cmd, callback)
-    pub = rospy.Publisher('sensor_data', SensorData, queue_size=10)
     rospy.on_shutdown(serial_port.close()) # closes the serial port when ROS gets the shutdown signal
     # Check if the node has received a signal to shut down. If not, run the method.
     try:
